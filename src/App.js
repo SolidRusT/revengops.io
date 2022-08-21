74dd13c850948534f04084f5e2b3b14647286f67
Shaun Prince <shaun@techfusion.ca>
2022-08-21 01:19:53 -0700

    learning where to put things

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          revengops.io says: nothing to see here... move along now.
         </p>
         <a
           className="App-link"
